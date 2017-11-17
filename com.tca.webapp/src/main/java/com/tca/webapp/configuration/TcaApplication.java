38c2713f994fef9c6cb431cc58f36f49134a26dc
kkaynar <kkaynar@gk-software.com>
2017-11-17 17:07:41 +0100

    refactoring resource for webjars

--- com.tca.webapp/src/main/java/com/tca/webapp/configuration/TcaApplication.java
+++ com.tca.webapp/src/main/java/com/tca/webapp/configuration/TcaApplication.java
@@ -4,13 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication(scanBasePackages = { "com.tca" })
 @EnableJpaRepositories(basePackages = "com.tca.dao.access")
 @EntityScan(basePackages = "com.tca.dao.model")
-public class TcaApplication {
+public class TcaApplication extends WebMvcConfigurerAdapter {
 
   public static void main(String[] args) {
     SpringApplication.run(TcaApplication.class, args);
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    if (!registry.hasMappingForPattern("/webjars/**")) {
+      registry.addResourceHandler("/webjars/**").addResourceLocations(
+          "classpath:/META-INF/resources/webjars/");
+    }
   }
 }
