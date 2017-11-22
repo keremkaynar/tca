7b709223bbd8adad63c310b03a5690772c0b62c3
kkaynar <kkaynar@gk-software.com>
2017-11-22 13:06:33 +0100

    web condiguration separated.

---
+++ com.tca.webapp/src/main/java/com/tca/webapp/configuration/TcaWebMvcConfiguration.java
@@ -0,0 +1,14 @@
+package com.tca.webapp.configuration;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class TcaWebMvcConfiguration extends WebMvcConfigurerAdapter {
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    if (!registry.hasMappingForPattern("/webjars/**")) {
+      registry.addResourceHandler("/webjars/**").addResourceLocations(
+          "classpath:/META-INF/resources/webjars/");
+    }
+  }
+}
